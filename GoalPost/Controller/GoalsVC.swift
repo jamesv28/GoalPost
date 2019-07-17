a96976870dadd7f96af43d9efd1755b8c623bdfb
James Volmert <james.volmert@gmail.com>
2019-07-17 14:34:36 -0600

    made the table view persist

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var goals: [Goal] = []
     
     @IBAction func addGoalPressed(_ sender: Any) {
-        
         guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
         presentDetail(createGoalVC)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -26,7 +28,22 @@ class GoalsVC: UIViewController {
         tableView.isHidden = false
         
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.fetch { (complete) in
+            if complete {
+                if goals.count >= 1 {
+                    tableView.isHidden = false
+                    
+                }
+                else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+        tableView.reloadData()
+    }
 
 }
 
@@ -37,12 +54,33 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell" ) as? GoalCell else { return UITableViewCell() }
-        cell.configureCell(description: "eat salad twice a week", type: .shortTerm, goalProgressAmt: 3)
+        let goal = goals[indexPath.row]
+        
+        cell.configureCell(description: goal.goalDescription!, type: GoalType(rawValue: goal.goalType!)!, goalProgressAmt: Int(goal.goalProgress))
         return cell
+    }
+}
+
+extension GoalsVC {
+    func fetch(completion: (_ complete: Bool) -> () ) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
+        
+        do {
+            // explicitly casting this as a goals array below see if this works
+            goals = try managedContext.fetch(fetchRequest)
+            print("success bitches")
+            completion(true)
+        }
+        catch {
+            debugPrint("Could not fetch \(error.localizedDescription)")
+        }
+        
+        
     }
 }
